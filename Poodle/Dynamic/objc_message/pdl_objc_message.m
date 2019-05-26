f8e8c779c7725d315cf913699410c368235ff55d
James Sun <rainbowsun1988@gmail.com>
2019-05-27 07:24:31 +0800

    .

--- Poodle/Dynamic/objc_message/pdl_objc_message.m
+++ Poodle/Dynamic/objc_message/pdl_objc_message.m
@@ -17,19 +17,24 @@ static void *pdl_malloc(size_t size) {
     return address;
 }
 
-static void pdl_objc_msgSend_before(__unsafe_unretained id self, SEL _cmd) {
+void pdl_objc_msgSend_before(__unsafe_unretained id self, SEL _cmd) {
     ;
 }
 
-static void pdl_objc_msgSend_after(__unsafe_unretained id self, SEL _cmd) {
+void pdl_objc_msgSend_after(__unsafe_unretained id self, SEL _cmd) {
+    ;
+}
+
+__attribute__ ((constructor))
+static void m(void) {
     ;
 }
 
 DYLD_INTERPOSE(pdl_malloc, malloc)
 
 DYLD_INTERPOSE(pdl_objc_msgSend, objc_msgSend)
-DYLD_INTERPOSE(pdl_objc_msgSendSuper, objc_msgSendSuper)
+//DYLD_INTERPOSE(pdl_objc_msgSendSuper, objc_msgSendSuper)
 #ifndef __arm64__
 DYLD_INTERPOSE(pdl_objc_msgSend_stret, objc_msgSend_stret)
-DYLD_INTERPOSE(pdl_objc_msgSendSuper_stret, objc_msgSendSuper_stret)
+//DYLD_INTERPOSE(pdl_objc_msgSendSuper_stret, objc_msgSendSuper_stret)
 #endif
