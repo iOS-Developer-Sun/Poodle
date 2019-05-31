a9339b25964362c7d4a8968d416c5a73b648b9c0
James Sun <rainbowsun1988@gmail.com>
2019-05-31 15:26:33 +0800

    .

---
+++ Poodle/UIKit/PDLKeyboardNotificationObserver/PDLKeyboardNotificationObserver.h
@@ -0,0 +1,37 @@
+//
+//  PDLKeyboardNotificationObserver.h
+//  Poodle
+//
+//  Created by Poodle on 25/09/2017.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol PDLKeyboardNotificationObserver;
+
+@interface PDLKeyboardNotificationObserver : NSObject
+
+@property (nonatomic, weak) id <PDLKeyboardNotificationObserver>delegate;
+@property (nonatomic, copy, readonly) NSDictionary *currentInfo;
+
++ (instancetype)observerForDelegate:(id <PDLKeyboardNotificationObserver>)delegate;
+
+- (void)startObserving;
+- (void)stopObserving;
+
+@end
+
+@protocol PDLKeyboardNotificationObserver <NSObject>
+
+@optional
+
+- (void)keyboardWillShow:(PDLKeyboardNotificationObserver *)observer;
+- (void)keyboardWillHide:(PDLKeyboardNotificationObserver *)observer;
+- (void)keyboardDidShow:(PDLKeyboardNotificationObserver *)observer;
+- (void)keyboardDidHide:(PDLKeyboardNotificationObserver *)observer;
+
+- (void)keyboardShowAnimation:(PDLKeyboardNotificationObserver *)observer withKeyboardHeight:(CGFloat)keyboardHeight;
+- (void)keyboardHideAnimation:(PDLKeyboardNotificationObserver *)observer withKeyboardHeight:(CGFloat)keyboardHeight;
+
+@end
---
+++ Poodle/UIKit/PDLKeyboardNotificationObserver/PDLKeyboardNotificationObserver.m
@@ -0,0 +1,129 @@
+//
+//  PDLKeyboardNotificationObserver.m
+//  Poodle
+//
+//  Created by Poodle on 25/09/2017.
+//
+//
+
+#import "PDLKeyboardNotificationObserver.h"
+
+@interface PDLKeyboardNotificationObserver ()
+
+@property (nonatomic, class, readonly) NSMapTable *observers;
+@property (nonatomic, assign) BOOL isObserving;
+
+@property (nonatomic, copy) NSDictionary *currentInfo;
+
+@end
+
+@implementation PDLKeyboardNotificationObserver
+
++ (NSMapTable *)observers {
+    static NSMapTable *observers = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        observers = [NSMapTable weakToStrongObjectsMapTable];
+    });
+    return observers;
+}
+
++ (instancetype)observerForDelegate:(id <PDLKeyboardNotificationObserver>)delegate {
+    NSMapTable *observers = self.observers;
+    PDLKeyboardNotificationObserver *observer = [observers objectForKey:delegate];
+    if (observer == nil) {
+        observer = [[self alloc] init];
+        observer.delegate = delegate;
+        [observers setObject:observer forKey:delegate];
+    }
+    return observer;
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)startObserving {
+    if (self.isObserving) {
+        return;
+    }
+
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleKeyboardWillShowNotification:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleKeyboardWillHideNotification:) name:UIKeyboardWillHideNotification object:nil];
+    self.isObserving = YES;
+}
+
+- (void)stopObserving {
+    if (self.isObserving == NO) {
+        return;
+    }
+
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+    self.isObserving = NO;
+}
+
+
+- (void)handleKeyboardWillShowNotification:(NSNotification *)notification {
+    if ([UIApplication sharedApplication].applicationState != UIApplicationStateActive) {
+        return;
+    }
+
+    self.currentInfo = notification.userInfo;
+
+    if ([self.delegate respondsToSelector:@selector(keyboardWillShow:)]) {
+        [self.delegate keyboardWillShow:self];
+    }
+
+    CGFloat height = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
+    NSTimeInterval animationDuration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    UIViewAnimationCurve curve = [notification.userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue];
+    [UIView beginAnimations:NSStringFromSelector(@selector(handleKeyboardWillShowNotification:)) context:nil];
+    [UIView setAnimationDuration:animationDuration];
+    [UIView setAnimationCurve:curve];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:context:)];
+    if ([self.delegate respondsToSelector:@selector(keyboardShowAnimation:withKeyboardHeight:)]) {
+        [self.delegate keyboardShowAnimation:self withKeyboardHeight:height];
+    }
+    [UIView commitAnimations];
+}
+
+- (void)handleKeyboardWillHideNotification:(NSNotification *)notification {
+    if ([UIApplication sharedApplication].applicationState != UIApplicationStateActive) {
+        return;
+    }
+
+    self.currentInfo = notification.userInfo;
+
+    if ([self.delegate respondsToSelector:@selector(keyboardWillHide:)]) {
+        [self.delegate keyboardWillHide:self];
+    }
+
+    CGFloat height = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
+    NSTimeInterval animationDuration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    UIViewAnimationCurve curve = [notification.userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue];
+    [UIView beginAnimations:NSStringFromSelector(@selector(handleKeyboardWillHideNotification:)) context:nil];
+    [UIView setAnimationDuration:animationDuration];
+    [UIView setAnimationCurve:curve];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:context:)];
+    if ([self.delegate respondsToSelector:@selector(keyboardHideAnimation:withKeyboardHeight:)]) {
+        [self.delegate keyboardHideAnimation:self withKeyboardHeight:height];
+    }
+    [UIView commitAnimations];
+}
+
+- (void)animationDidStop:(NSString *)animationID finished:(NSNumber *)finished context:(void *)context {
+    if ([animationID isEqualToString:NSStringFromSelector(@selector(handleKeyboardWillShowNotification:))]) {
+        if ([self.delegate respondsToSelector:@selector(keyboardDidShow:)]) {
+            [self.delegate keyboardDidShow:self];
+        }
+    } else if ([animationID isEqualToString:NSStringFromSelector(@selector(handleKeyboardWillHideNotification:))]) {
+        if ([self.delegate respondsToSelector:@selector(keyboardDidHide:)]) {
+            [self.delegate keyboardDidHide:self];
+        }
+    }
+}
+
+@end
