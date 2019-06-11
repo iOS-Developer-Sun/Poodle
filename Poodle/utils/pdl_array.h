1a509a923de68f261819b38316d4996e0ebbbfa3
James Sun <rainbowsun1988@gmail.com>
2019-06-11 17:11:45 +0800

    .

--- Poodle/utils/pdl_array.h
+++ Poodle/utils/pdl_array.h
@@ -9,7 +9,6 @@ extern void pdl_addObject(pdl_array_t array, void *object);
 extern void pdl_insertObjectAtIndex(pdl_array_t array, void *object, unsigned int index);
 extern pdl_array_t pdl_copyArray(pdl_array_t array);
 extern void pdl_destroyArray(pdl_array_t array);
-extern void pdl_destroyArrayWithFunction(pdl_array_t array, void (*objectFunction)(void *));
 extern void pdl_sortByFunction(pdl_array_t array, int(*sort)(void *object1, void *object2));
 extern void pdl_sortByFunctionAndData(pdl_array_t array, int(*sort)(void *object1, void *object2, void *data), void *data);
 extern unsigned int pdl_countOfArray(pdl_array_t array);
--- Poodle/utils/pdl_dictionary.h
+++ Poodle/utils/pdl_dictionary.h
@@ -11,6 +11,5 @@ extern pdl_dictionary_t pdl_copyDictionary(pdl_dictionary_t dictionary);
 extern pdl_array_t pdl_allKeys(pdl_dictionary_t dictionary);
 extern void pdl_getAllKeys(pdl_dictionary_t dictionary, void ***keys, unsigned int *count);
 extern void pdl_destroyDictionary(pdl_dictionary_t dictionary);
-extern void pdl_destroyDictionaryWithFunctions(pdl_dictionary_t dictionary, void (*keyFunction)(void *), void (*objectFunction)(void *));
 extern unsigned int pdl_countOfDictionary(pdl_dictionary_t dictionary);
 extern void pdl_printDictionary(pdl_dictionary_t dictionary);
