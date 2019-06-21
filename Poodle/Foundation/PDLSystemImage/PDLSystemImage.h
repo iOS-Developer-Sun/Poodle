939ca9850705ee53600b8477c886c1817ff60ee5
sun <250764090@qq.com>
2019-06-21 17:20:10 +0800

    .

---
+++ Poodle/Foundation/PDLSystemImage/PDLSystemImage.h
@@ -0,0 +1,40 @@
+//
+//  PDLSystemImage.h
+//  Poodle
+//
+//  Created by Poodle on 22/09/2017.
+//  
+//
+
+#import <Foundation/Foundation.h>
+#include <mach-o/loader.h>
+#include <mach/machine.h>
+
+@interface PDLSystemImage : NSObject
+
+@property (class, readonly) NSUInteger count;
+
+@property (readonly) NSString *name;
+@property (readonly) NSString *path;
+@property (readonly) uint64_t vmsize;
+@property (readonly) uintptr_t address;
+@property (readonly) uintptr_t endAddress;
+@property (readonly) uintptr_t vmAddress;
+@property (readonly) cpu_type_t cpuType;
+@property (readonly) cpu_subtype_t cpuSubtype;
+@property (readonly) const uint8_t *uuid;
+@property (readonly) uint64_t majorVersion;
+@property (readonly) uint64_t minorVersion;
+@property (readonly) uint64_t revisionVersion;
+
+@property (readonly) NSString *uuidString;
+@property (readonly) NSString *cpuTypeString;
+@property (readonly) NSString *crashLogString;
+
++ (instancetype)systemImageAtIndex:(NSUInteger)index;
++ (instancetype)systemImageWithPath:(NSString *)path;
++ (instancetype)systemImageWithHeader:(struct mach_header *)header;
++ (NSArray *)systemImages;
++ (NSArray *)systemImagesSortedByAddress;
+
+@end
