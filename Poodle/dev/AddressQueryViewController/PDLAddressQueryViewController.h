737936fd30af69efecd38d4eb351c3035869647f
James Sun <rainbowsun1988@gmail.com>
2019-08-03 17:30:37 +0800

    .

---
+++ Poodle/dev/AddressQueryViewController/PDLAddressQueryViewController.h
@@ -0,0 +1,13 @@
+//
+//  PDLAddressQueryViewController.h
+//  Poodle
+//
+//  Created by Poodle on 15/4/28.
+//  Copyright © 2019 Poodle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDLAddressQueryViewController : UIViewController
+
+@end
---
+++ Poodle/dev/MemoryQueryViewController/PDLMemoryQueryViewController.h
@@ -0,0 +1,52 @@
+//
+//  PDLMemoryQueryViewController.h
+//  Poodle
+//
+//  Created by Poodle on 15/7/16.
+//  Copyright © 2019 Poodle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSUInteger, PDLMemoryQueryResultType) {
+    PDLMemoryQueryResultTypeUnknown,
+    PDLMemoryQueryResultTypeVoid,
+    PDLMemoryQueryResultTypeClass,
+    PDLMemoryQueryResultTypeSelector,
+    PDLMemoryQueryResultTypeNSObject,
+    PDLMemoryQueryResultTypeChar,
+    PDLMemoryQueryResultTypeShort,
+    PDLMemoryQueryResultTypeInt,
+    PDLMemoryQueryResultTypeLong,
+    PDLMemoryQueryResultTypeLongLong,
+    PDLMemoryQueryResultTypeUnsignedChar,
+    PDLMemoryQueryResultTypeUnsignedShort,
+    PDLMemoryQueryResultTypeUnsignedInt,
+    PDLMemoryQueryResultTypeUnsignedLong,
+    PDLMemoryQueryResultTypeUnsignedLongLong,
+    PDLMemoryQueryResultTypeFloat,
+    PDLMemoryQueryResultTypeDouble,
+    PDLMemoryQueryResultTypeBool,
+    PDLMemoryQueryResultTypePointer,
+    PDLMemoryQueryResultTypeFunctionPointer,
+    PDLMemoryQueryResultTypeCString,
+    PDLMemoryQueryResultTypeBlock,
+    PDLMemoryQueryResultTypeCGPoint,
+    PDLMemoryQueryResultTypeCGSize,
+    PDLMemoryQueryResultTypeCGRect,
+    PDLMemoryQueryResultTypeError,
+    PDLMemoryQueryResultTypeCount
+};
+
+@interface PDLMemoryQueryResult : NSObject
+
+@property (nonatomic, assign) PDLMemoryQueryResultType type;
+@property (nonatomic, strong) id result;
+
+@end
+
+@interface PDLMemoryQueryViewController : UIViewController
+
++ (void)addConstantQueryWithTitle:(NSString *)title action:(void(^)(PDLMemoryQueryResult *result))action;
+
+@end
