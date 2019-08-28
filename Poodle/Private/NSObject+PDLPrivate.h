c1e0bd1b5e687e01aa2f7fdc56f8f1bf82954e3f
sunzj <sunzijian@kuaishou.com>
2019-08-28 18:03:25 +0800

    .

--- Poodle/Private/NSObject+PDLPrivate.h
+++ Poodle/Private/NSObject+PDLPrivate.h
@@ -23,15 +23,21 @@
 extern id objc_retain(__unsafe_unretained id object);
 extern void objc_release(__unsafe_unretained id object);
 extern id objc_autorelease(__unsafe_unretained id object);
+
 extern void _objc_autoreleasePoolPrint(void);
 
 extern id objc_storeStrong(__unsafe_unretained id *location, __unsafe_unretained id object);
-extern void objc_copyStruct(void *dest, const void *src, ptrdiff_t size, BOOL atomic, BOOL hasStrong __unused);
-extern id objc_getProperty(__unsafe_unretained id self, SEL _cmd, ptrdiff_t offset, BOOL atomic);
-extern void objc_setProperty_atomic(__unsafe_unretained id self, SEL _cmd, __unsafe_unretained id newValue, ptrdiff_t offset);
 extern id objc_loadWeakRetained(__unsafe_unretained id *location);
+
+extern id objc_getProperty(__unsafe_unretained id self, SEL _cmd, ptrdiff_t offset, BOOL atomic);
+extern void objc_setProperty_nonatomic(__unsafe_unretained id self, SEL _cmd, __unsafe_unretained id newValue, ptrdiff_t offset);
+extern void objc_setProperty(__unsafe_unretained id self, SEL _cmd, ptrdiff_t offset, __unsafe_unretained id newValue, BOOL atomic, char shouldCopy);
+
+extern void objc_setProperty_atomic(__unsafe_unretained id self, SEL _cmd, __unsafe_unretained id newValue, ptrdiff_t offset);
 extern void objc_setProperty_nonatomic_copy(__unsafe_unretained id self, SEL _cmd, __unsafe_unretained id newValue, ptrdiff_t offset);
 extern void objc_setProperty_atomic_copy(__unsafe_unretained id self, SEL _cmd, __unsafe_unretained id newValue, ptrdiff_t offset);
+
+extern void objc_copyStruct(void *dest, const void *src, ptrdiff_t size, BOOL atomic, BOOL hasStrong __unused);
 
 OBJC_EXPORT void
 objc_msgSendSuper2(void /* struct objc_super *super, SEL op, ... */ )
