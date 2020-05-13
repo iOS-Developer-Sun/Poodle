91fe09c7482d4a7c6ea5d15b6cc282702005e149
James Sun <rainbowsun1988@gmail.com>
2020-05-13 20:40:28 +0800

    .

--- Poodle/pdl_backtrace/pdl_backtrace.c
+++ Poodle/pdl_backtrace/pdl_backtrace.c
@@ -34,7 +34,12 @@ int pdl_backtrace_stack(void *link_register, void *frame_pointer, void **frames,
             break;
         }
         fp = *fp;
-        lr = *(fp + 1);
+
+        if (fp) {
+            lr = *(fp + 1);
+        } else {
+            lr = NULL;
+        }
     }
     return ret;
 #else
@@ -51,41 +56,47 @@ typedef struct pdl_backtrace {
     int frames_count;
     pthread_t thread;
     pthread_mutex_t lock;
+    pthread_mutex_t wait_lock;
     char thread_name[MAXTHREADNAMESIZE];
 } pdl_backtrace;
 
+void pdl_backtrace_wait(pdl_backtrace *bt) {
+    pthread_mutex_t *wait_lock = &(bt->wait_lock);
+    pthread_mutex_unlock(wait_lock);
+
+    pthread_mutex_t *lock = &(bt->lock);
+    pthread_mutex_lock(lock);
+    pthread_mutex_lock(lock);
+    pthread_mutex_unlock(lock);
+}
+
 #ifdef __arm64__
 __attribute__((naked))
-static void pdl_backtrace_fake(__unused void **frames, __unused pthread_mutex_t *lock) {
-    __asm__ volatile ("sub sp, sp, #0x40\n" // new space
-                      "stp x19, x20, [sp, #0x10]\n"
-                      "stp x21, x22, [sp, #0x20]\n"
-                      "stp x29, x30, [sp, #0x30]\n"
+static void pdl_backtrace_fake(__unused pdl_backtrace *bt, __unused void **frames) {
+    __asm__ volatile (
+                      "sub sp, sp, #0x20\n" // new space
+                      "stp x19, x20, [sp, #0x10]\n" // backup caller registers
+                      "mov x19, fp\n" // store fp
+                      "mov fp, x1\n" // fake frames
 
-                      "mov x19, x0\n" // store frames
-                      "mov x20, x1\n" // store lock
-                      "mov x21, fp\n" // store fp
-                      "mov fp, x0\n" // fake
-                      "mov x0, x20\n" // load lock
-                      "bl _pthread_mutex_lock\n" // pthread_mutex_lock(lock);
-                      "mov fp, x21\n" // recover
+                      "bl _pdl_backtrace_wait\n" // wait pdl_backtrace_wait(bt);
 
-                      "ldp x19, x20, [sp, #0x10]\n" // delete space
-                      "ldp x21, x22, [sp, #0x20]\n"
-                      "ldp x29, x30, [sp, #0x30]\n"
-                      "add sp, sp, #0x40\n"
-                      "ret");
+                      "mov fp, x19\n" // recover frames
+
+                      "ldp x19, x20, [sp, #0x10]\n" // restore caller registers
+                      "add sp, sp, #0x20\n" // delete space
+                      "ret"
+                      );
 }
 #else
-static void pdl_backtrace_fake(void **frames, pthread_mutex_t *lock) {
-    pthread_mutex_lock(lock);
+static void pdl_backtrace_fake(pdl_backtrace *bt, __unused void **frames) {
+    pdl_backtrace_wait(bt);
 }
 #endif
 
 static void *pdl_backtrace_thread_main(void *backtrace) {
     pdl_backtrace *bt = (pdl_backtrace_t)backtrace;
     pthread_setname_np(bt->thread_name);
-    pthread_mutex_t *lock = &(bt->lock);
     void **frames = bt->malloc_ptr(sizeof(void *) * PDL_BACKTRACE_FRAMES_MAX_COUNT * 2);
     for (int i = 0; i < bt->frames_count; i++) {
         int fp_index = i * 2;
@@ -98,9 +109,7 @@ static void *pdl_backtrace_thread_main(void *backtrace) {
         frames[lr_index] = bt->frames[i];
     }
 
-    pthread_mutex_lock(lock);
-    pdl_backtrace_fake(frames, lock);
-    pthread_mutex_unlock(lock);
+    pdl_backtrace_fake(bt, frames);
     bt->free_ptr(frames);
 
     return NULL;
@@ -121,13 +130,16 @@ pdl_backtrace_t pdl_backtrace_create_with_malloc_pointers(void *(*malloc_ptr)(si
     bt->free_ptr = f_ptr;
     pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
     bt->lock = lock;
+    pthread_mutex_t wait_lock = PTHREAD_MUTEX_INITIALIZER;
+    bt->wait_lock = wait_lock;
+    strlcpy(bt->thread_name, "pdl_backtrace", sizeof(bt->thread_name));
 
     return bt;
 }
 
-void pdl_backtrace_set_name(pdl_backtrace_t backtrace, char *name) {
+void pdl_backtrace_set_name(pdl_backtrace_t backtrace, const char *name) {
     pdl_backtrace *bt = (pdl_backtrace_t)backtrace;
-    strlcpy(bt->thread_name, name ?: "pdl_backtrace", sizeof(bt->thread_name));
+    strlcpy(bt->thread_name, name ?: "", sizeof(bt->thread_name));
 }
 
 void pdl_backtrace_record(pdl_backtrace_t backtrace) {
@@ -147,17 +159,29 @@ void pdl_backtrace_record(pdl_backtrace_t backtrace) {
     bt->frames_count = count_recorded;
 }
 
-void pdl_backtrace_thread_show(pdl_backtrace_t backtrace) {
+void pdl_backtrace_thread_show(pdl_backtrace_t backtrace, bool wait) {
     pdl_backtrace *bt = (pdl_backtrace_t)backtrace;
     if (bt->thread) {
         return;
     }
 
+    pthread_mutex_t *wait_lock = &(bt->wait_lock);
+    pthread_mutex_lock(wait_lock);
     pthread_t thread = 0;
     int ret = pthread_create(&thread, NULL, &pdl_backtrace_thread_main, bt);
     if (ret == 0) {
         bt->thread = thread;
     }
+
+    if (wait) {
+        pthread_mutex_lock(wait_lock);
+        pthread_mutex_unlock(wait_lock);
+    }
+}
+
+bool pdl_backtrace_thread_is_shown(pdl_backtrace_t backtrace) {
+    pdl_backtrace *bt = (pdl_backtrace_t)backtrace;
+    return (bt->thread != NULL);
 }
 
 void pdl_backtrace_thread_hide(pdl_backtrace_t backtrace) {
