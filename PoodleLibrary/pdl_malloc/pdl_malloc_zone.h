f7272c7e705809313f2386f7fecf4108c014ea11
James Sun <rainbowsun1988@gmail.com>
2020-05-21 19:52:59 +0800

    .

---
+++ PoodleLibrary/pdl_malloc/pdl_malloc_zone.h
@@ -0,0 +1,22 @@
+//
+//  pdl_malloc_zone.h
+//  Poodle
+//
+//  Created by Poodle on 2020/5/15.
+//  Copyright © 2019 Poodle. All rights reserved.
+//
+
+#include <malloc/malloc.h>
+#include <stdbool.h>
+
+extern malloc_zone_t *pdl_malloc_zone(void);
+extern void *pdl_malloc_zone_malloc(size_t size);
+extern void *pdl_malloc_zone_realloc(void *ptr, size_t size);
+extern void pdl_malloc_zone_free(void *ptr);
+
+extern void pdl_malloc_check_pointer(void *pointer);
+extern void pdl_malloc_zone_enumerate(malloc_zone_t *zone, void *data, void(*function)(void *data, vm_range_t range, unsigned int type, unsigned int count, unsigned int index, bool *stops));
+
+extern bool pdl_malloc_assert_enabled(void);
+extern void pdl_malloc_assert_set_enabled(bool enabled);
+extern bool pdl_malloc_enable_trace(void);
