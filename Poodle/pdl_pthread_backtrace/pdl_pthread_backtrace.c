26c9479916ff690f4a7dfcbc9956d3390f6b1c68
James Sun <rainbowsun1988@gmail.com>
2020-06-04 19:49:21 +0800

    .

--- Poodle/pdl_pthread_backtrace/pdl_pthread_backtrace.c
+++ Poodle/pdl_pthread_backtrace/pdl_pthread_backtrace.c
@@ -9,24 +9,22 @@
 #import "pdl_pthread_backtrace.h"
 #import "pdl_backtrace.h"
 
-typedef struct pdl_thread_info {
+typedef struct pdl_pthread_info {
     void *(*start)(void *);
     void *arg;
     pdl_backtrace_t backtrace;
-//    int (*pthread_create_original)(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
-} pdl_thread_info;
+} pdl_pthread_info;
 
-static void *pdl_thread_start(void *arg) {
-    pdl_thread_info *info = (typeof(info))arg;
-//    pdl_backtrace_thread_show_with_start(info->backtrace, true, info->pthread_create_original);
-    void *ret = pdl_backtrace_thread_execute(info->backtrace, info->start, info->arg);
+static void *pdl_pthread_start(void *arg) {
+    pdl_pthread_info *info = (typeof(info))arg;
+    void *ret = pdl_backtrace_thread_execute(info->backtrace, info->start, info->arg, false);
     pdl_backtrace_destroy(info->backtrace);
     free(info);
     return ret;
 }
 
 int pdl_pthread_backtrace_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg, int (*pthread_create_original)(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg)) {
-    pdl_thread_info *info = malloc(sizeof(pdl_thread_info));
+    pdl_pthread_info *info = malloc(sizeof(pdl_pthread_info));
     int ret = 0;
     if (!info) {
         ret = pthread_create_original(thread, attr, start_routine, arg);
@@ -38,8 +36,7 @@ int pdl_pthread_backtrace_create(pthread_t *thread, const pthread_attr_t *attr, 
         }
         info->start = start_routine;
         info->arg = arg;
-//        info->pthread_create_original = pthread_create_original;
-        ret = pthread_create_original(thread, attr, &pdl_thread_start, info);
+        ret = pthread_create_original(thread, attr, &pdl_pthread_start, info);
         if (ret != 0) {
             free(info);
         }
