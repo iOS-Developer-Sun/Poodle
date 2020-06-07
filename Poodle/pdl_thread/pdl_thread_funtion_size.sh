c10caf6f2872b7c8fc1aa7b0587035c748b116a4
James Sun <rainbowsun1988@gmail.com>
2020-06-07 14:39:58 +0800

    .

--- Poodle/pdl_thread/pdl_thread_funtion_size.sh
+++ Poodle/pdl_thread/pdl_thread_funtion_size.sh
@@ -7,17 +7,18 @@ function pdl_thread () {
     out=$4
 
     path="${arch}/${file}"
-    echo ${CONFIGURATION} ${arch} >> ${out}
+    echo ${CONFIGURATION} ${arch} > ${out}
 
     if [ -f $path ]; then
         nm -n -radix=d "${path}" | grep -A 1 ${sym} >> ${out}
         objdump -u "${path}" | grep -A 1 ${sym} >> ${out}
+        objdump -df=${sym} -d $path >> ${out}
     fi
 }
 
 scripts="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-out="${scripts}/${CONFIGURATION}_${arch}.txt"
+folder="${scripts}/pdl_thread_funtion_size"
 
 cd ${OBJECT_FILE_DIR_normal}
 
@@ -26,20 +27,20 @@ file="pdl_thread.o"
 
 if [[ $PLATFORM_NAME == "iphonesimulator" ]]; then
     arch="i386"
-    out="${scripts}/pdl_thread_${CONFIGURATION}_${arch}.txt"
+    out="${folder}/${CONFIGURATION}_${arch}.txt"
     pdl_thread ${arch} ${file} ${sym} ${out}
 
     arch="x86_64"
-    out="${scripts}/pdl_thread_${CONFIGURATION}_${arch}.txt"
+    out="${folder}/${CONFIGURATION}_${arch}.txt"
     pdl_thread ${arch} ${file} ${sym} ${out}
 fi
 
 if [[ $PLATFORM_NAME == "iphoneos" ]]; then
     arch="armv7"
-    out="${scripts}/pdl_thread_${CONFIGURATION}_${arch}.txt"
+    out="${folder}/${CONFIGURATION}_${arch}.txt"
     pdl_thread ${arch} ${file} ${sym} ${out}
 
     arch="arm64"
-    out="${scripts}/pdl_thread_${CONFIGURATION}_${arch}.txt"
+    out="${folder}/${CONFIGURATION}_${arch}.txt"
     pdl_thread ${arch} ${file} ${sym} ${out}
 fi
