28a3533d248c4fd040b7038cdcba3807ddac1b85
James Sun <rainbowsun1988@gmail.com>
2020-07-23 18:09:58 +0800

    .

--- PoodleLibrary/NSObject+PDLAssociation/NSObject+PDLAssociation.h
+++ PoodleLibrary/NSObject+PDLAssociation/NSObject+PDLAssociation.h
@@ -13,26 +13,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSObject (PDLAssociation)
 
-@property (nonatomic, strong, setter=pdl_setNonatomicStrongAssociatedObject:) id _Nullable pdl_nonatomicStrongAssociatedObject;
-@property (atomic, strong, setter=pdl_setStrongAssociatedObject:) id _Nullable pdl_strongAssociatedObject;
+- (id _Nullable)pdl_associatedObjectForKey:(const void *)key;
+- (void)pdl_setAssociatedObject:(id _Nullable)object forKey:(const void *)key policy:(objc_AssociationPolicy)policy;
 
-@property (nonatomic, copy, setter=pdl_setNonatomicCopyAssociatedObject:) id _Nullable pdl_nonatomicCopyAssociatedObject;
-@property (atomic, copy, setter=pdl_setCopyAssociatedObject:) id _Nullable pdl_copyAssociatedObject;
+- (id _Nullable)pdl_nonatomicWeakAssociatedObjectForKey:(const void *)key;
+- (void)pdl_setNonatomicWeakAssociatedObject:(id _Nullable)object forKey:(const void *)key;
 
-@property (unsafe_unretained, setter=pdl_setUnsafeUnretainedAssociatedObject:) id _Nullable pdl_unsafeUnretainedAssociatedObject;
-
-- (id _Nullable)pdl_associatedObjectForKey:(void *)key;
-- (void)pdl_setAssociatedObject:(id _Nullable)object forKey:(void *)key policy:(objc_AssociationPolicy)policy;
-
-#pragma mark - weak
-
-@property (nonatomic, weak, setter=pdl_setNonatomicWeakAssociatedObject:) id _Nullable pdl_nonatomicWeakAssociatedObject;
-@property (atomic, weak, setter=pdl_setWeakAssociatedObject:) id _Nullable pdl_weakAssociatedObject;
-
-- (id _Nullable)pdl_nonatomicWeakAssociatedObjectForKey:(void *)key;
-- (void)pdl_setNonatomicWeakAssociatedObject:(id _Nullable)object forKey:(void *)key;
-- (id _Nullable)pdl_weakAssociatedObjectForKey:(void *)key;
-- (void)pdl_setWeakAssociatedObject:(id _Nullable)object forKey:(void *)key;
+- (id _Nullable)pdl_weakAssociatedObjectForKey:(const void *)key;
+- (void)pdl_setWeakAssociatedObject:(id _Nullable)object forKey:(const void *)key;
 
 @end
 
