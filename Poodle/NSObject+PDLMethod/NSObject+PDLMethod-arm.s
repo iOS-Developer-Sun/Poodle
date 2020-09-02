bcd0ae621fc56dc573de52331b81a5628d3ace54
sunzj <sunzijian@kuaishou.com>
2020-09-02 18:38:34 +0800

    .

--- Poodle/NSObject+PDLMethod/NSObject+PDLMethod-arm.s
+++ Poodle/NSObject+PDLMethod/NSObject+PDLMethod-arm.s
@@ -13,9 +13,63 @@
 .text
 .align 4
 .private_extern _PDLMethodEntry
+.private_extern _PDLMethodEntry_stret
+.private_extern _PDLMethodEntryFull
+.private_extern _PDLMethodEntryFull_stret
 
 _PDLMethodEntry:
 
-nop
+    PDL_ASM_OBJC_MESSAGE_STATE_SAVE NORMAL
+    bl      _PDLMethodBefore
+    PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
+
+    ldr     ip, [r1, #0x8]
+    ldr     r1, [r1]
+    bx      ip
+
+_PDLMethodEntry_stret:
+    PDL_ASM_OBJC_MESSAGE_STATE_SAVE STRET
+    bl      _PDLMethodBefore
+    PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
+
+    ldr     ip, [r2, #0x8]
+    ldr     r2, [r2]
+    bx      ip
+
+_PDLMethodEntryFull:
+
+    PDL_ASM_OBJC_MESSAGE_STATE_SAVE NORMAL
+    mov     r2, lr
+    bl      _PDLMethodFullBefore
+    PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
+
+    ldr     ip, [r1, #0x8]
+    ldr     r1, [r1]
+    blx     ip
+
+    PDL_ASM_OBJC_MESSAGE_STATE_SAVE NORMAL
+    bl      _PDLMethodFullAfter
+    mov     ip, r0
+    PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
+    mov     lr, ip
+    mov     pc, lr
+
+_PDLMethodEntryFull_stret:
+
+    PDL_ASM_OBJC_MESSAGE_STATE_SAVE STRET
+    mov     r2, lr
+    bl      _PDLMethodFullBefore
+    PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
+
+    ldr     ip, [r2, #0x8]
+    ldr     r2, [r2]
+    blx     ip
+
+    PDL_ASM_OBJC_MESSAGE_STATE_SAVE STRET
+    bl      _PDLMethodFullAfter
+    mov     ip, r0
+    PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
+    mov     lr, ip
+    mov     pc, lr
 
 #endif
