b40c7a8cd14cdb10f301f1fe86a8a26b14e4a7e4
James Sun <rainbowsun1988@gmail.com>
2020-09-02 11:44:07 +0800

    .

--- Poodle/NSObject+PDLMethod/NSObject+PDLMethod-arm64.s
+++ Poodle/NSObject+PDLMethod/NSObject+PDLMethod-arm64.s
@@ -13,35 +13,43 @@
 .text
 .align 4
 .private_extern _PDLMethodEntry
+.private_extern _PDLMethodEntry_stret
 .private_extern _PDLMethodEntryFull
+.private_extern _PDLMethodEntryFull_stret
 
 _PDLMethodEntry:
 
-PDL_ASM_OBJC_MESSAGE_STATE_SAVE
-bl     _PDLMethodBefore
-PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
+    PDL_ASM_OBJC_MESSAGE_STATE_SAVE
+    bl      _PDLMethodBefore
+    PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
 
-ldr     x9, [x1, #0x10]
-ldr     x1, [x1]
-br     x9
+    ldr     x9, [x1, #0x10]
+    ldr     x1, [x1]
+    br      x9
+
+_PDLMethodEntry_stret:
+    b   _PDLMethodEntry
 
 _PDLMethodEntryFull:
 
-PDL_ASM_OBJC_MESSAGE_STATE_SAVE
-mov    x2, lr
-bl     _PDLMethodFullBefore
-PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
+    PDL_ASM_OBJC_MESSAGE_STATE_SAVE
+    mov     x2, lr
+    bl      _PDLMethodFullBefore
+    PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
 
-ldr     x9, [x1, #0x10]
-ldr     x1, [x1]
-blr     x9
+    ldr     x9, [x1, #0x10]
+    ldr     x1, [x1]
+    blr     x9
 
-PDL_ASM_OBJC_MESSAGE_STATE_SAVE
-bl      _PDLMethodFullAfter
-mov     x9, x0
-PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
-mov     lr, x9
+    PDL_ASM_OBJC_MESSAGE_STATE_SAVE
+    bl      _PDLMethodFullAfter
+    mov     x9, x0
+    PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
+    mov     lr, x9
 
-ret
+    ret
+
+_PDLMethodEntryFull_stret:
+    b   _PDLMethodEntryFull
 
 #endif
--- Poodle/NSObject+PDLMethod/NSObject+PDLMethod-x86_64.s
+++ Poodle/NSObject+PDLMethod/NSObject+PDLMethod-x86_64.s
@@ -8,14 +8,75 @@
 
 #include "pdl_asm-universal.h"
 
-#ifdef __x64_64__
+#ifdef __x86_64__
 
 .text
 .align 4
 .private_extern _PDLMethodEntry
+.private_extern _PDLMethodEntry_stret
+.private_extern _PDLMethodEntryFull
+.private_extern _PDLMethodEntryFull_stret
 
 _PDLMethodEntry:
 
-nop
+    PDL_ASM_OBJC_MESSAGE_STATE_SAVE NORMAL
+    call    _PDLMethodBefore
+    PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
+
+    movq    0x10(%rsi), %rax
+    movq    (%rsi), %rsi
+    jmp     *%rax
+
+_PDLMethodEntry_stret:
+
+    PDL_ASM_OBJC_MESSAGE_STATE_SAVE STRET
+    call    _PDLMethodBefore
+    PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
+
+    movq    0x10(%rdx), %rax
+    movq    (%rdx), %rdx
+    jmp     *%rax
+
+_PDLMethodEntryFull:
+
+    PDL_ASM_OBJC_MESSAGE_STATE_SAVE NORMAL
+    movq    0x8(%rbp), %rdx     // save lr
+    call    _PDLMethodFullBefore
+    PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
+
+    popq    %rax    // fake sp begin
+
+    movq    0x10(%rsi), %rax    // fetch imp
+    movq    (%rsi), %rsi    //  switch arg
+
+    call    *%rax   // call imp
+
+    pushq   %rax    // save ret
+
+    call    _PDLMethodFullAfter
+    movq    %rax, %r11  // save lr to r11
+    popq    %rax        // load ret
+
+    movq    %r11, 0x8(%rsp)    // restore lr
+    popq    %r11    // fake sp end
+
+    ret
+
+_PDLMethodEntryFull_stret:
+
+    PDL_ASM_OBJC_MESSAGE_STATE_SAVE STRET
+    movq    0x8(%rbp), %rdx
+    call    _PDLMethodFullBefore
+    PDL_ASM_OBJC_MESSAGE_STATE_RESTORE
+
+    movq    0x10(%rdx), %rax
+    movq    (%rdx), %rdx
+    call    *%rax
+
+    pushq   %rax
+    call    _PDLMethodFullAfter
+    popq   %rax
+
+    ret
 
 #endif
