67344f0f2594db3883d110947a32330a44dccdd1
sunzj <sunzijian@kuaishou.com>
2020-09-03 11:54:17 +0800

    .

--- Poodle/pdl_asm/pdl_asm.h
+++ Poodle/pdl_asm/pdl_asm.h
@@ -9,3 +9,13 @@
 #ifndef PDL_ASM_NOP
 #define PDL_ASM_NOP __asm__ volatile ("nop")
 #endif
+
+#ifndef PDL_ASM_GOTO
+#if defined(__i386__) || defined(__x86_64__)
+#define PDL_ASM_GOTO(function) __asm__ volatile ("jmp _" #function)
+#elif defined(__arm__) || defined(__arm64__)
+#define PDL_ASM_GOTO(function) __asm__ volatile ("b _" #function)
+#else
+#error
+#endif
+#endif
