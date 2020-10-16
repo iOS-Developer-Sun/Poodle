59af033069220ea653cf398bbdc453c3dc3d6de2
James Sun <rainbowsun1988@gmail.com>
2020-10-16 15:27:51 +0800

    .

--- Poodle.rb
+++ Poodle.rb
@@ -300,6 +300,14 @@ def PoodleSpec(name, path: nil, is_library: false, default_subspec: nil)
             ss.dependency pod_name + '/pdl_backtrace'
         end
 
+        s.subspec 'pdl_dynamic' do |ss|
+            ss.platform = platform_universal
+            ss.osx.deployment_target  = '10.10'
+            ss.ios.deployment_target  = '9.0'
+            ss.source_files = base + 'pdl_dynamic/' + source_files
+            ss.vendored_library = base + 'pdl_dynamic/' + librariy_files
+        end
+
         s.subspec 'pdl_lldb_hook' do |ss|
             ss.platform = platform_universal
             ss.osx.deployment_target  = '10.10'
