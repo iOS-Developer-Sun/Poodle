d55abfae0aa11db022b4b246db49a1bc5f4db595
James Sun <rainbowsun1988@gmail.com>
2021-02-10 16:30:59 +0800

    .

---
+++ Poodle/PDLBlock/PDLBlock.h
@@ -0,0 +1,17 @@
+//
+//  PDLBlock.h
+//  Poodle
+//
+//  Created by Poodle on 2021/2/3.
+//  Copyright © 2021 Poodle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PDLBlock : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
