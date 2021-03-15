9976bce08c8f29987be13adfebb01034c637b80d
James Sun <rainbowsun1988@gmail.com>
2021-03-15 22:21:40 +0800

    .

--- Poodle/PDLLoad/PDLLoad.c
+++ Poodle/PDLLoad/PDLLoad.c
@@ -1,5 +1,5 @@
 //
-//  PDLLoad.m
+//  PDLLoad.c
 //  Poodle
 //
 //  Created by Poodle on 2020/12/22.
@@ -11,24 +11,22 @@
 #import <mach-o/ldsyms.h>
 #import <mach-o/dyld.h>
 
-@implementation PDLLoad
-
 static void emptyLoad(__unsafe_unretained id self, SEL _cmd) {
     return;
 }
 
-+ (void)disableCategoryLoad:(BOOL(^_Nullable)(void *imageHeader, NSString *imageName, Class aClass, NSString *categoryName))filter {
+void pdl_disableCategoryLoad(bool(*filter)(void *image_header, const char *image_name, Class cls, const char *category_name)) {
     if (!filter) {
         return;
     }
 
     uint32_t count = _dyld_image_count();
     for (uint32_t i = 0; i < count; i++) {
-        void *imageHeader = (void *)_dyld_get_image_header(i);
-        const char *imageName = _dyld_get_image_name(i);
+        void *image_header = (void *)_dyld_get_image_header(i);
+        const char *image_name = _dyld_get_image_name(i);
 
         size_t count = 0;
-        pdl_objc_runtime_category *categories = pdl_objc_runtime_categories(imageHeader, &count);
+        pdl_objc_runtime_category *categories = pdl_objc_runtime_categories(image_header, &count);
         SEL selLoad = sel_registerName("load");
         for (size_t i = 0; i < count; i++) {
             pdl_objc_runtime_category *category = categories[i];
@@ -37,7 +35,7 @@ static void emptyLoad(__unsafe_unretained id self, SEL _cmd) {
             }
 
             const char *name = pdl_objc_runtime_category_get_name(category);
-            Class aClass = pdl_objc_runtime_category_get_class(category);
+            Class cls = pdl_objc_runtime_category_get_class(category);
             pdl_objc_runtime_method_list method_list = pdl_objc_runtime_category_get_class_method_list(category);
             if (!method_list) {
                 continue;
@@ -51,7 +49,7 @@ static void emptyLoad(__unsafe_unretained id self, SEL _cmd) {
                     continue;
                 }
 
-                BOOL disabled = filter(imageHeader, @(imageName), aClass, @(name));
+                bool disabled = filter(image_header, image_name, cls, name);
                 if (disabled) {
                     method_setImplementation(method, (IMP)&emptyLoad);
                 }
@@ -59,5 +57,3 @@ static void emptyLoad(__unsafe_unretained id self, SEL _cmd) {
         }
     }
 }
-
-@end
--- Poodle/pdl_objc_runtime/pdl_objc_runtime.c
+++ Poodle/pdl_objc_runtime/pdl_objc_runtime.c
@@ -1,5 +1,5 @@
 //
-//  pdl_objc_runtime.m
+//  pdl_objc_runtime.c
 //  Poodle
 //
 //  Created by Poodle on 2020/6/18.
@@ -8,22 +8,6 @@
 
 #import "pdl_objc_runtime.h"
 #import <mach-o/getsect.h>
-
-__attribute__((ns_returns_retained))
-id pdl_class_createInstance(__unsafe_unretained Class cls, size_t extraBytes, __attribute__((ns_returns_retained)) id(* class_createInstance_original)(__unsafe_unretained Class cls, size_t extraBytes)) {
-    return class_createInstance_original(cls, extraBytes);
-}
-
-__attribute__((ns_returns_retained))
-id pdl_objc_rootAllocWithZone(__unsafe_unretained Class cls, struct _NSZone *zone, id(* _objc_rootAllocWithZone_original)(__unsafe_unretained Class cls, struct _NSZone *zone)) {
-    return _objc_rootAllocWithZone_original(cls, zone);
-}
-
-void pdl_objc_rootDealloc(__unsafe_unretained id object, void(* _objc_rootDealloc_original)(__unsafe_unretained id object)) {
-    _objc_rootDealloc_original(object);
-}
-
-#pragma mark -
 
 struct method_t {
     SEL name;
