00c60a9c5630a191fb973615476c943e17f1f126
James Sun <rainbowsun1988@gmail.com>
2021-04-09 15:00:05 +0800

    .

---
+++ Poodle/NSObject+PDLDescription/NSObject+PDLDescription.m
@@ -0,0 +1,22 @@
+//
+//  NSObject+PDLDescription.m
+//  Poodle
+//
+//  Created by Poodle on 2020/6/8.
+//  Copyright © 2020 Poodle. All rights reserved.
+//
+
+#import "NSObject+PDLDescription.h"
+
+@implementation NSObject (PDLDescription)
+
+- (NSString *)pdl_description {
+    NSDictionary *dictionary = nil;
+    NSString *description = self.description;
+    if (dictionary) {
+        ;
+    }
+    return description;
+}
+
+@end
---
+++ PoodleLibrary/NSObject+PDLDescription/NSObject+PDLDescription.h
@@ -0,0 +1,19 @@
+//
+//  NSObject+PDLDescription.h
+//  Poodle
+//
+//  Created by Poodle on 2020/6/8.
+//  Copyright © 2020 Poodle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (PDLDescription)
+
+@property (readonly) NSString *pdl_description;
+
+@end
+
+NS_ASSUME_NONNULL_END
