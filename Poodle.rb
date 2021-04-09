f7bbac49efb2062b20ce05f40ed1ab2bc7e5c8df
James Sun <rainbowsun1988@gmail.com>
2021-04-09 18:16:08 +0800

    .

--- Poodle.rb
+++ Poodle.rb
@@ -148,6 +148,15 @@ def PoodleSpec(name, path: nil, is_library: false, default_subspec: nil)
             ss.requires_arc = ['NSObject+PDLDebug/NSObject+PDLDebug.m']
         end
 
+        s.subspec 'NSObject+PDLDescription' do |ss|
+            ss.platform = platform_universal
+            ss.osx.deployment_target  = '10.10'
+            ss.ios.deployment_target  = '9.0'
+            ss.source_files = base + 'NSObject+PDLDescription/' + source_files
+            ss.vendored_library = base + 'NSObject+PDLDescription/' + librariy_files
+            ss.frameworks = 'Foundation'
+        end
+
         s.subspec 'NSObject+PDLExtension' do |ss|
             ss.platform = platform_universal
             ss.osx.deployment_target  = '10.10'
