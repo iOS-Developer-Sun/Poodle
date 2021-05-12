3393d98589e41af6ed6df63c4a572494a6b516f2
James Sun <rainbowsun1988@gmail.com>
2021-05-12 20:39:56 +0800

    .

--- PoodleLibrary/NSObject+PDLWeakifyUnsafeUnretainedProperty/NSObject+PDLWeakifyUnsafeUnretainedProperty.h
+++ PoodleLibrary/NSObject+PDLWeakifyUnsafeUnretainedProperty/NSObject+PDLWeakifyUnsafeUnretainedProperty.h
@@ -12,6 +12,13 @@
 
 + (BOOL)pdl_weakifyUnsafeUnretainedProperty:(NSString *)propertyName;
 + (BOOL)pdl_weakifyUnsafeUnretainedProperty:(NSString *)propertyName ivarName:(NSString *)ivarName;
+
+/// make an unsafe unretained property weak, implementations of setter and getter will be replaced.
+/// @discussion the property must not have custom implementation.
+/// @param propertyName propertyName
+/// @param ivarName ivar name
+/// @param ivarClass class the ivar is in
+/// @return YES if succeeded; NO if propertyName or ivarName length is invalid or getter and setter do not exist.
 + (BOOL)pdl_weakifyUnsafeUnretainedProperty:(NSString *)propertyName ivarName:(NSString *)ivarName ivarClass:(Class)ivarClass;
 
 @end
