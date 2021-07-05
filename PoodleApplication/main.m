03b74d4dc7415f527eb22d89eed59f46502fb3ba
James Sun <rainbowsun1988@gmail.com>
2021-07-05 17:12:21 +0800

    .

--- PoodleApplication/main.m
+++ PoodleApplication/main.m
@@ -10,5 +10,6 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-    return UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    return 0;
+//    return UIApplicationMain(argc, argv, nil, @"AppDelegate");
 }
