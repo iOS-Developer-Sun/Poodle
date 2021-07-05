b9e0f310700b47f748d91b9a4be9933177fc4888
James Sun <rainbowsun1988@gmail.com>
2021-07-05 11:46:10 +0800

    .

---
+++ Poodle/PDLPudding/PDLPudding.m
@@ -0,0 +1,21 @@
+//
+//  PDLPudding.m
+//  Poodle
+//
+//  Created by sunzj on 2021/7/5.
+//  Copyright © 2021 Poodle. All rights reserved.
+//
+
+#import "PDLPudding.h"
+#import <objc/runtime.h>
+#import <objc/message.h>
+
+// JSPatch 1.1.3
+
+@implementation PDLPudding
+
++ (void)enable {
+    ;
+}
+
+@end
