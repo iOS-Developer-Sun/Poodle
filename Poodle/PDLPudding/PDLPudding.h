4f72644cae4185fb1fd9bd38c256886b4a750f49
James Sun <rainbowsun1988@gmail.com>
2021-07-05 11:46:10 +0800

    .

---
+++ Poodle/PDLPudding/PDLPudding.h
@@ -0,0 +1,19 @@
+//
+//  PDLPudding.h
+//  Poodle
+//
+//  Created by sunzj on 2021/7/5.
+//  Copyright © 2021 Poodle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PDLPudding : NSObject
+
++ (void)enable;
+
+@end
+
+NS_ASSUME_NONNULL_END
