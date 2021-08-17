08c25fa48020507583f6b6a516547e5b959b7dae
James Sun <rainbowsun1988@gmail.com>
2021-08-17 18:26:58 +0800

    .

--- PoodleLibrary/pdl_asm/pdl_asm.h
+++ PoodleLibrary/pdl_asm/pdl_asm.h
@@ -19,3 +19,74 @@
 #error
 #endif
 #endif
+
+
+#ifndef PDL_ASM_PERFORM
+
+#define PDL_ASM_PERFORM(instruction) __asm__ volatile (instruction)
+
+#define PDL_ASM_PERFORM_2(instruction) \
+PDL_ASM_PERFORM(instruction);\
+PDL_ASM_PERFORM(instruction)
+
+#define PDL_ASM_PERFORM_4(instruction) \
+PDL_ASM_PERFORM_2(instruction);\
+PDL_ASM_PERFORM_2(instruction)
+
+#define PDL_ASM_PERFORM_8(instruction) \
+PDL_ASM_PERFORM_4(instruction);\
+PDL_ASM_PERFORM_4(instruction)
+
+#define PDL_ASM_PERFORM_16(instruction) \
+PDL_ASM_PERFORM_8(instruction);\
+PDL_ASM_PERFORM_8(instruction)
+
+#define PDL_ASM_PERFORM_32(instruction) \
+PDL_ASM_PERFORM_16(instruction);\
+PDL_ASM_PERFORM_16(instruction)
+
+#define PDL_ASM_PERFORM_64(instruction) \
+PDL_ASM_PERFORM_32(instruction);\
+PDL_ASM_PERFORM_32(instruction)
+
+#define PDL_ASM_PERFORM_128(instruction) \
+PDL_ASM_PERFORM_64(instruction);\
+PDL_ASM_PERFORM_64(instruction)
+
+#define PDL_ASM_PERFORM_256(instruction) \
+PDL_ASM_PERFORM_128(instruction);\
+PDL_ASM_PERFORM_128(instruction)
+
+#define PDL_ASM_PERFORM_512(instruction) \
+PDL_ASM_PERFORM_256(instruction);\
+PDL_ASM_PERFORM_256(instruction)
+
+#define PDL_ASM_PERFORM_1024(instruction) \
+PDL_ASM_PERFORM_512(instruction);\
+PDL_ASM_PERFORM_512(instruction)
+
+#define PDL_ASM_PERFORM_2048(instruction) \
+PDL_ASM_PERFORM_1024(instruction);\
+PDL_ASM_PERFORM_1024(instruction)
+
+#define PDL_ASM_PERFORM_4096(instruction) \
+PDL_ASM_PERFORM_2048(instruction);\
+PDL_ASM_PERFORM_2048(instruction)
+
+#define PDL_ASM_PERFORM_8192(instruction) \
+PDL_ASM_PERFORM_4096(instruction);\
+PDL_ASM_PERFORM_4096(instruction)
+
+#define PDL_ASM_PERFORM_16384(instruction) \
+PDL_ASM_PERFORM_8192(instruction);\
+PDL_ASM_PERFORM_8192(instruction)
+
+#define PDL_ASM_PERFORM_32768(instruction) \
+PDL_ASM_PERFORM_16384(instruction);\
+PDL_ASM_PERFORM_16384(instruction)
+
+#define PDL_ASM_PERFORM_65536(instruction) \
+PDL_ASM_PERFORM_32768(instruction);\
+PDL_ASM_PERFORM_32768(instruction)
+
+#endif
