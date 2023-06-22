1955ca8b30cbec42a5193834fb2f46a78565fa20
James Sun <rainbowsun1988@gmail.com>
2023-06-22 17:00:03 +0800

    .

---
+++ PoodleLibrary/PDLNonThreadSafeObserver/PDLNonThreadSafeArrayObserverObject.h
@@ -0,0 +1,17 @@
+//
+//  PDLNonThreadSafeArrayObserverObject.h
+//  Poodle
+//
+//  Created by Poodle on 2020/1/14.
+//  Copyright © 2020 Poodle. All rights reserved.
+//
+
+#import "PDLNonThreadSafeClusterObserverObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PDLNonThreadSafeArrayObserverObject : PDLNonThreadSafeClusterObserverObject
+
+@end
+
+NS_ASSUME_NONNULL_END
