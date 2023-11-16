60b7221abebae858345336806beb12df635cf3ea
James Sun <rainbowsun1988@gmail.com>
2023-11-16 10:58:06 +0800

    .

---
+++ PoodleLibrary/pdl_trampoline/pdl_trampoline.h
@@ -0,0 +1,19 @@
+//
+//  pdl_trampoline.h
+//  Poodle
+//
+//  Created by Poodle on 11-8-23.
+//  Copyright © 2021 Poodle. All rights reserved.
+//
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void *pdl_trampoline(void *original, void(*before)(void *original, void *data), void(*after)(void *original, void *data), void *data);
+
+#ifdef __cplusplus
+}
+#endif
